75a0b46a1fd0755722bf1246de7f90096f2d0897
sabamiso11 <adachi.business.11@gmail.com>
2018-05-21 14:59:02 +0900

    correct migrate

--- database/migrations/2018_05_16_190605_create_tasks_table.php
+++ database/migrations/2018_05_16_190605_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('list_id');
+            $table->unsignedInteger('task_list_id');
             $table->string('task_name');
             $table->date('limit');
             $table->boolean('done')->default(false);
