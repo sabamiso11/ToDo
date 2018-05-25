f46fd65489ff9a8579e7acf41310c2e40fc487a3
sabamiso11 <adachi.business.11@gmail.com>
2018-05-25 15:25:32 +0900

    remove debugbar

--- config/app.php
+++ config/app.php
@@ -180,7 +180,7 @@ return [
 	 /*
          * Package Service Providers...
          */
-        Barryvdh\Debugbar\ServiceProvider::class,
+        //Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
